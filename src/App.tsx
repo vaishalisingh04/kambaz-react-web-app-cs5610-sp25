414e2bea113befbeff71be1cf8d67022ba3338f6
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-04-03 06:55:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Kanbas from "./kanbas";
+import Kambaz from "./kanbas";
 import Labs from "./Labs";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import store from "./kanbas/store";
@@ -12,7 +12,7 @@ function App() {
         <div>
           <Routes>
             <Route path="/Labs/*" element={<Labs />} />
-            <Route path="/Kanbas/*" element={<Kanbas />} />
+            <Route path="/Kanbas/*" element={<Kambaz />} />
             <Route path="/" element={<Navigate to="/Labs" />} />
           </Routes>
         </div>
