cb7435661e37c0525c4a96b5d4e7258e46c62b8b
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-04-03 07:37:43 -0400

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -39,8 +39,8 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="#/Kanbas" className="nav-link">
-          Kanbas
+        <a id="wd-k" href="#/Kambaz" className="nav-link">
+          Kambaz
         </a>
       </li>
     </ul>
